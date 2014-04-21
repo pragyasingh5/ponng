f7f7bbfde486c9ff6385be1a7893cfd84010ebda
pragya5 <pragya.singh.mk@gmail.com>
2014-04-20 19:14:36 -0700

    Create ViewController.h

---
+++ ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  pong
+//
+//  Created by Pragya Singh on 4/19/14.
+//  Copyright (c) 2014 Pragya Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+{
+    
+    IBOutlet UIImageView *AnimatedBackground;
+    
+}
+@end
